4bc433fdd64fe1b1adbfbe001e5bddfc4f49b626
Raife Stokes <raifestokes@gmail.com>
2013-05-01 18:14:39 +1000

    add remove nested contacts

--- app/models/client.rb
+++ app/models/client.rb
@@ -17,9 +17,9 @@
 
 class Client < ActiveRecord::Base
   default_scope  order('name ASC')
-  attr_accessible :address, :city, :default_billing_rate, :default_billing_unit, :name, :phone, :state, :postcode, :contacts_attributes
-  has_many :contacts
-  accepts_nested_attributes_for :contacts
+  attr_accessible :address, :city, :default_billing_rate, :default_billing_unit, :name, :phone, :state, :postcode, :contacts_attributes, :allow_destroy => true
+  has_many :contacts, :dependent => :destroy
+  accepts_nested_attributes_for :contacts, :allow_destroy => true
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
   validates :address, presence: true, length: { maximum: 50 }
   validates :city, presence: true, length: { maximum: 50 }
