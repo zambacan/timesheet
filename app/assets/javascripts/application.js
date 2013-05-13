8279946ebb130bdba550dfb7914a0a9035a1197d
Raife Stokes <raifestokes@gmail.com>
2013-05-13 22:44:54 +1000

    precompile assets in production config

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,56 +18,6 @@
 
 
    
-$(document).ready(function() {
-
-	var date = new Date();
-	var d = date.getDate();
-	var m = date.getMonth();
-	var y = date.getFullYear();
-	
-	var calendar = $('#calendar').fullCalendar({
-		header: {
-			left: 'prev,next today',
-			center: 'title',
-			right: 'month,agendaWeek,agendaDay'
-		},
-		editable: true,
-		theme: true,
-		allDaySlot: false,
-		selectable: true,
-		selectHelper: true,
-		select: function(start, end, allDay) {
-			var title = prompt('Event Title:');
-			if (title) {
-				calendar.fullCalendar('renderEvent',
-					{
-						title: title,
-						start: start,
-						end: end,
-						allDay: allDay
-					},
-					true // make the event "stick"
-				);
-			}
-			calendar.fullCalendar('unselect');
-		},
-		
-	});
-	
-});
-
-function updateEvent(the_event) {
-    $.update(
-      "/events/" + the_event.id,
-      { event: { title: the_event.title,
-                 starts_at: "" + the_event.start,
-                 ends_at: "" + the_event.end,
-                 description: the_event.description
-               }
-      },
-      function (reponse) { alert('successfully updated task.'); }
-    );
-};
 
 
 
