cdfb40418701c8f4647a169a04e8eb7434de50ef
Raife Stokes <raifestokes@gmail.com>
2013-05-19 21:08:42 +1000

    tidy up merge conflict

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
+	$('.timepicker').timepicker({
+	     minuteStep: 15,
+	     showInputs: false,
+	     disableFocus: true
+	 });
 
-	var date = new Date();
-	var d = date.getDate();
-	var m = date.getMonth();
-	var y = date.getFullYear();
+	// var date       = new Date();
+	// var d          = date.getDate();
+	// var m          = date.getMonth();
+	// var y          = date.getFullYear();
 	
-	var calendar = $('#calendar').fullCalendar({
+	var calendar   = $('#calendar').fullCalendar({
 		header: {
 			left: 'prev,next today',
 			center: 'title',
@@ -17,7 +22,18 @@ $(document).ready(function() {
 		selectable: true,
 		selectHelper: true,
 		select: function(start, end, allDay) {
-			var title = prompt('Event Title:');
+			$('#myModal').modal('show');
+		    $('#myModal').on('shown', function() {
+			var st = formatTime(start);
+			var ft = formatTime(end);
+
+		         $("#startTime").val(st);
+		         $("#finishTime").val(ft);
+		         $("#performedOn").val(start);
+		        // 		
+		    })
+			  // var title = "brill"
+			// prompt('Raify Title:');
 			if (title) {
 				calendar.fullCalendar('renderEvent',
 					{
@@ -31,11 +47,35 @@ $(document).ready(function() {
 			}
 			calendar.fullCalendar('unselect');
 		},
+				
 		
 	});
-	
+
+
 });
 
+ 	 function formatTime(givenTime) {
+	    var hours   = givenTime.getHours();
+	    var minutes = givenTime.getMinutes();
+	    if (hours  > 12) {
+			hours   = hours - 12;
+		var ap = ' PM';	
+			}
+			else
+			{
+			var ap=' AM';	
+			}
+	
+	    if (hours   === 0) {
+		hours   = "12";
+		ap=" AM";}
+		if (hours   === 12) {
+		ap=" PM";}
+	    if (hours   < 10) {hours   = "0"+hours;}
+	    if (minutes < 10) {minutes = "0"+minutes;}
+	    var time    = hours+':'+minutes+ap;
+	    return time;
+	}
 function updateEvent(the_event) {
     $.update(
       "/events/" + the_event.id,
@@ -47,4 +87,6 @@ function updateEvent(the_event) {
       },
       function (reponse) { alert('successfully updated task.'); }
     );
+
+
 };
