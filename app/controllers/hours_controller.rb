e784ef8416075e975cfe7c9facd0d1235afdd0a1
Raife Stokes <raifestokes@gmail.com>
2013-05-20 22:36:58 +1000

    add basic time recording

--- app/controllers/hours_controller.rb
+++ app/controllers/hours_controller.rb
@@ -1,5 +1,7 @@
 class HoursController < ApplicationController
   
+
+  
   def new
     @hour=Hour.new
   end
@@ -7,4 +9,35 @@ class HoursController < ApplicationController
   def index
    @hours=Hour.all
   end
+  
+  def create
+    @hours = Hour.new(params[:hour])
+
+    respond_to do |format|
+      if @hours.save
+        # flash[:success] = "Timesheet updated"
+        format.html { redirect_to hours_path, notice: 'Timesheet was successfully updated.' }
+        format.json { render json: @hours, status: :created, location: @project }
+      else
+        format.html { render action: "new"}
+        format.json { render json: @hours.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  
+  def destroy
+    @hour=Hour.find(params[:id])
+    @hour.destroy
+
+    respond_to do |format|
+      format.html { redirect_to hours_url }
+      format.json { head :no_content }
+    end
+  end
+  
+  
+  
 end
+
+
--- app/models/hour.rb
+++ app/models/hour.rb
@@ -19,19 +19,19 @@
 #
 
 class Hour < ActiveRecord::Base
-  attr_accessible :activity_type, :billable_unit_quantity, :billing_rate, :billing_status, :billing_unit, :finish_time, :performed_on, :project_name, :start_time, :user_id
+  attr_accessible :activity_type, :description, :billable_unit_quantity, :billing_rate, :billing_status, :billing_unit, :finish_time, :performed_on, :project_name, :start_time, :user_id
  belongs_to :project
 # activity_types include: consulting, meetings, coding, testing, business development
 # billing_status include: unbilled, billed, non-billable
 # billing_unit includes: hours, days, fixed-price
 
 def project_name
-  project.try(:name)
+  project.try(:project_name)
 end
 
 
-def project_name=(name)
-  self.project=Project.find_by_project_name(name) if name.present?
+def project_name=(project_name)
+  self.project=Project.find_by_project_name(project_name) if project_name.present?
 end
 
 end
